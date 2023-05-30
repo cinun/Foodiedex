19135db310c69633492ee612d7acdc2b01c7a5ce
cinun <yathartha.regmi@ttu.edu>
2023-05-30 08:14:45 -0700

    New: Initial Commit

---
+++ Foodiedex/FoodiedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodiedexApp.swift
+//  Foodiedex
+//
+//  Created by Yathartha Regmi on 5/30/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodiedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
