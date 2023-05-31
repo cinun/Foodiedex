c32e07136a72594cca4601bfeab0ced7f2a111a6
cinun <yathartha.regmi@ttu.edu>
2023-05-31 09:16:38 -0700

    Added initial version of the swipe feature along with sample pics

--- Foodiedex/ContentView.swift
+++ Foodiedex/ContentView.swift
@@ -8,15 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var translation: CGSize = .zero
+    @State private var swipeStatus: SwipeStatus = .left
+    @State private var currentImageIndex = 0
+
+
+    let imageDirectory = "/Users/yathartharegmi/Startup/Foodiedex/Foodiedex/Assets.xcassets"
+    var imageNames: [String] {
+        guard let imageFiles = Bundle.main.urls(forResourcesWithExtension: "jpg", subdirectory: "Pics") else {
+                return []
+            }
+            return imageFiles.map { $0.lastPathComponent }
+        }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+          VStack {
+              if imageNames.isEmpty {
+                  Text("No images found")
+              }
+              else if currentImageIndex >= 0 && currentImageIndex < imageNames.count {
+                  // Need to strip .jpg because assets dont have the .jpg extension in their folder
+                  let parsedImageName = imageNames[currentImageIndex].replacingOccurrences(of: ".jpg", with: "")
+                  Image(parsedImageName)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        //.frame(width: 300, height: 300)
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                        .offset(x: translation.width, y: translation.height)
+                        .gesture(
+                          DragGesture()
+                              .onChanged { gesture in
+                                  translation = gesture.translation
+                              }
+                              .onEnded { gesture in
+                                  let thresholdPercentage: CGFloat = 0.4
+                                  if abs(translation.width) > UIScreen.main.bounds.width * thresholdPercentage {
+                                      swipeStatus = translation.width > 0 ? .right : .left
+                                      updateImage()
+                                  } else {
+                                      translation = .zero
+                                  }
+                              }
+                        )
+                        .animation(.spring(), value: 0.8)
+                        .transition(.slide)
+              }
+              
+              Text(swipeStatus.rawValue)
+                  .font(.title)
+                  .padding()
+              
         }
         .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black.opacity(0.8).edgesIgnoringSafeArea(.all))
     }
+    
+    func updateImage() {
+        currentImageIndex = (currentImageIndex + 1) % imageNames.count
+    }
+}
+
+enum SwipeStatus: String{
+    case none, left, right
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ Foodiedex/TestView.swift
@@ -0,0 +1,20 @@
+//
+//  TestView.swift
+//  Foodiedex
+//
+//  Created by Yathartha Regmi on 5/31/23.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}
